33234b34bc92114d15936396001e72d194cd2288
Med-z <55351063+Med-z@users.noreply.github.com>
2020-04-02 23:06:17 +0200

    code inutile enlevé

--- src/Site/SiteBundle/Controller/DefaultController.php
+++ src/Site/SiteBundle/Controller/DefaultController.php
@@ -8,27 +8,6 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-
-        // variable globale déclarée dans le fichier config.yml
-
-        // on récupère la variable
-
-        $global = $this->get("twig")->getGlobals();
-        $identifiant = $global["identifiant"];
-        if($identifiant == 0){
-            return $this->render('@SiteSite/Default/index.html.twig', array(
-                'id' => $identifiant
-            ));
-
-        } else if($identifiant == 1){
-            return $this->render('@SiteSite/Default/index.html.twig', array(
-                'id' => $identifiant
-            ));
-
-        } else if ($identifiant == 2){
-            return $this->render('@SiteSite/Default/index.html.twig', array(
-                'id' => $identifiant
-            ));
-        }
+        return $this->render('@SiteSite/Default/index.html.twig');
     }
 }
