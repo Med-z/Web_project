2f656720a0397f56a5062c33d656ec2029e18938
Med-z <55351063+Med-z@users.noreply.github.com>
2020-04-03 01:59:25 +0200

    TEST POUR RECUPERER DONNEES UTILISATEURS

--- src/Site/SiteBundle/Controller/AdminController.php
+++ src/Site/SiteBundle/Controller/AdminController.php
@@ -3,13 +3,24 @@
 namespace Site\SiteBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class AdminController extends Controller
 {
-    public function editUsersAction()
+    public function editUsersAction(Request $request)
     {
-        $em = $this->getDoctrine()->getRepository('SiteSiteBundle:Users');
+
+        $em = $this->getDoctrine()->getRepository('SiteSiteBundle:Im1920Utilisateurs');
         $users = $em->findAll();
+
+        if($request->isMethod('post')){
+            $getId = $request->get('user.id');
+            $em = $this->getDoctrine()->getManager();
+            $user = $em->getRepository('SiteSiteBundle:Im1920Utilisateurs')->find($getId);
+            $em->remove($user);
+            $em->flush();
+        }
+
         return $this->render('@SiteSite/Admin/administration.html.twig', array(
             'users' => $users
         ));
