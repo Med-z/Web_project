527006368fd06f0c450ceba5d609fc8b150a4902
Med-z <55351063+Med-z@users.noreply.github.com>
2020-04-03 10:24:45 +0200

    deletion users

--- src/Site/SiteBundle/Controller/AdminController.php
+++ src/Site/SiteBundle/Controller/AdminController.php
@@ -13,17 +13,34 @@ class AdminController extends Controller
         $em = $this->getDoctrine()->getRepository('SiteSiteBundle:Im1920Utilisateurs');
         $users = $em->findAll();
 
-        if($request->isMethod('post')){
-            $getId = $request->get('user.id');
-            $em = $this->getDoctrine()->getManager();
-            $user = $em->getRepository('SiteSiteBundle:Im1920Utilisateurs')->find($getId);
-            $em->remove($user);
-            $em->flush();
-        }
-
         return $this->render('@SiteSite/Admin/administration.html.twig', array(
             'users' => $users
         ));
+    }
+
+    public function deleteAction(Request $request, $id)
+    {
+            $em = $this->getDoctrine()->getManager();
+            $prd_cmd = $em->getRepository('SiteSiteBundle:Im1920Prod_cmd')->findAll();
+            $pr = $em->getRepository('SiteSiteBundle:Im1920Product');
+            foreach($prd_cmd as $p){
+                if($p->getCmdId() == $id){
+                    $produit = $pr->findById($p->getProdId());
+                    $produit[0]->setAmount($produit[0]->getAmount() + $p->getQte());
+                    $em->remove($p);
+                }
+            }
+            $users = $em->getRepository('SiteSiteBundle:Im1920Utilisateurs')->findAll();
+            foreach($users as $user){
+                if($user->getId() == $id){
+                    $em->remove($user);
+                    $em->flush();
+                    return $this->render('@SiteSite/Admin/suppression.html.twig', array(
+                        'id' => $id
+                    ));
+                }
+            }
+
     }
 }
 
